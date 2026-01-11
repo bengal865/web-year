00de09d7c6b76143b402b1f5675cf5eecc3c7cb2
bengal865 <69059209+bengal865@users.noreply.github.com>
2026-01-11 05:35:30 -0500

    Revise exercise title and add TODOs for tasks
    Updated exercise title and added TODO comments for student code.

--- unit-3-js/script.js
+++ unit-3-js/script.js
@@ -3,7 +3,7 @@
 // Project Title
 
 /* =========================================================
-   UNIT: Title
+   EXERCISE 1: Variables
    Instructions: Only write code inside the TODO blocks.
    ========================================================= 
 */
@@ -14,16 +14,21 @@ const myButton = document.getElementById('action-button');
 
 myButton.addEventListener('click', () => {
     // TODO: Write a console.log message or an alert here
+    // === STUDENT CODE START === //
     
+
+    // === STUDENT CODE END === //
 });
 
 
 // --- TASK 2: CHANGING TEXT ---
-// Instructions
+/ TODO: Use heading.textContent to change the title
 function updateText() {
     const heading = document.querySelector('h1');
-    // TODO: Use heading.textContent to change the title
+    // === STUDENT CODE START === //
     
+
+    // === STUDENT CODE END === //
 }
 
 
